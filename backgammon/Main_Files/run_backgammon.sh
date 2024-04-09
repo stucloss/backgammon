d4db1cf9916c8b030f7a02bdd3c90807fdcd27be
stucloss <157414472+stucloss@users.noreply.github.com>
2024-04-09 07:53:57 -0400

    Create run_backgammon.sh

---
+++ backgammon/Main_Files/run_backgammon.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Create a virtual display
+Xvfb :1 -screen 0 1024x768x24 &
+
+# Set the DISPLAY environment variable to point to the virtual display
+export DISPLAY=:1
+
+# Run your Python script
+python3 /home/ec2-user/backgammon/backgammon/Main.py
