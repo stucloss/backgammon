f8079337f2ea674f19e68b0e60efce181cb87cd4
stucloss <157414472+stucloss@users.noreply.github.com>
2024-04-09 08:06:14 -0400

    Update run_backgammon.sh

--- backgammon/run_backgammon.sh
+++ backgammon/run_backgammon.sh
@@ -3,7 +3,7 @@
 Xvfb :1 -screen 0 1024x768x24 &
 
 # Set the DISPLAY environment variable to point to the virtual display
-export DISPLAY=:1 myapp
+export DISPLAY=:1 chromium-browser
 
 # Run your Python script
 python3 /home/ec2-user/backgammon/backgammon/Main.py
